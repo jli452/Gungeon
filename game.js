6cc806d5818fd36000c15c2ebf899804c6682a71
jli37 <jli37@stuy.edu>
2019-05-04 15:44:52 -0400

    still working on pause fuctionality??? why cant i get it????

--- game.js
+++ game.js
@@ -9,6 +9,8 @@ var rightPressed = false;
 var leftPressed = false;
 var upPressed = false;
 var downPressed = false;
+var escPressed = false;
+var escCounter = 0;
 
 var charPosX = (canvas.width/2 - charRadius);
 var charPosY = (canvas.height/2 - charRadius);
@@ -16,33 +18,33 @@ var charPosY = (canvas.height/2 - charRadius);
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
-// Keycodes for WASD
-// W: 87
-// A: 65
-// S: 83
-// D: 68
 
 function keyUpHandler(event) {
-  if (event.keyCode === 68) {
+  if (event.key === "d") {
     rightPressed = false;
-  } else if (event.keyCode === 65) {
+  } else if (event.key === "a") {
     leftPressed = false;
-  } else if (event.keyCode === 87) {
+  } else if (event.key === "w") {
     upPressed = false;
-  } else if (event.keyCode === 83) {
+  } else if (event.key === "s") {
     downPressed = false;
+  } else if (event.key === "Escape") {
+    escPressed = false;
   }
 }
 
 function keyDownHandler(event) {
-  if (event.keyCode === 68) {
+  if (event.key === "d") {
     rightPressed = true;
-  } else if (event.keyCode === 65) {
+  } else if (event.key === "a") {
     leftPressed = true;
-  } else if (event.keyCode === 87) {
+  } else if (event.key === "w") {
     upPressed = true;
-  } else if (event.keyCode === 83) {
+  } else if (event.key === "s") {
     downPressed = true;
+  } else if (event.key === "Escape") {
+    escPressed = true;
+    escCounter ++;
   }
 }
 
@@ -56,35 +58,53 @@ function drawChar() {
 
 function playerMovement() {
   if (rightPressed && upPressed && charPosX < canvas.width && charPosY > 0) {
-    charPosX += 3;
-    charPosY -=3;
+    charPosX += 2;
+    charPosY -= 2;
   }
   else if(leftPressed && upPressed && charPosX > 0 && charPosY > 0) {
-    charPosX -= 3;
-    charPosY -=3;
+    charPosX -= 2;
+    charPosY -= 2;
   }
   else if(rightPressed && downPressed && charPosX < canvas.width && charPosY < canvas.height) {
-    charPosX += 3;
-    charPosY +=3;
+    charPosX += 2;
+    charPosY += 2;
   }
   else if(leftPressed && downPressed && charPosY < canvas.height && charPosX > 0) {
-    charPosX -= 3;
-    charPosY +=3;
+    charPosX -= 2;
+    charPosY +=2;
   }
   else if (rightPressed && charPosX < canvas.width) {
-    charPosX += 3;
+    charPosX += 2;
   } else if (leftPressed && charPosX > 0) {
-    charPosX -= 3;
+    charPosX -= 2;
   } else if (upPressed && charPosY > 0) {
-    charPosY -= 3;
+    charPosY -= 2;
   } else if (downPressed && charPosY < canvas.height) {
-    charPosY += 3;
+    charPosY += 2;
   }
 }
+
+var isRunning = true;
+var isPaused = escCounter % 2;
+
+function pauseGame() {
+  if (escCounter > 0 && escPressed) {
+    ctx.font = "40px Arial";
+    ctx.fillStyle = "white";
+    ctx.fillText("PAUSED", 80, 650);
+    isRunning = !isRunning;
+  }
+}
+
 
 function playGame() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawChar();
   playerMovement();
+  pauseGame();
+  if (isRunning) {
+    requestAnimationFrame(playGame);
+  }
 }
-var interval = setInterval(playGame, 10);
+
+playGame();
