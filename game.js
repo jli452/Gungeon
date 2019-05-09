33b58c7167ace3b9904a677c3eb9484ccfc35114
jli37 <43329325+jli37@users.noreply.github.com>
2019-05-09 14:05:23 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,3 +1,10 @@
+
+// TODO:
+// DRAW GUN AS RECTANGLE
+// MAKE GUN FOLLOW CHARACTER
+
+
+
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
@@ -5,6 +12,7 @@ var startGame = false;
 var gamePaused = false;
 
 var charRadius = 20;
+var gunPosition = charRadius/2;
 
 var rightPressed = false;
 var leftPressed = false;
@@ -13,15 +21,23 @@ var downPressed = false;
 var escPressed = false;
 var mouseClicked = false;
 
+var char = {
+  posX: (canvas.width / 2),
+  posY: (canvas.height / 2)
 
-var charPosX = (canvas.width / 2 - charRadius);
-var charPosY = (canvas.height / 2 - charRadius);
+};
+
+var gun = {
+  posX: (canvas.width / 2),
+  posY: (canvas.height / 2)
+}
+
+
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 document.addEventListener("mousedown", mouseDownHandler, false);
 document.addEventListener("mouseup", mouseUpHandler, false);
-
 
 // Checking which keys are not being pressed
 function keyUpHandler(event) {
@@ -38,19 +54,29 @@ function keyUpHandler(event) {
   }
 }
 
+
+
+
 // Cheecking which keys are being pressed
 function keyDownHandler(event) {
   if (event.key === "d") {
     rightPressed = true;
+    gun.posX = gun.posX - gunPosition;
   } else if (event.key === "a") {
     leftPressed = true;
+    gun.posX = gun.posX + gunPosition;
   } else if (event.key === "w") {
     upPressed = true;
+    gun.posY = gun.posY - gunPosition;
   } else if (event.key === "s") {
     downPressed = true;
+    gun.posY = gun.posY + gunPosition;
   } else if (event.key === "Escape") {
     escPressed = true;
-    pauseGame()
+    pauseGame();
+  } else {
+    gun.posX = char.posX;
+    gun.posY = char.posY;
   }
 }
 
@@ -69,33 +95,33 @@ function mouseDownHandler(event) {
 
 function drawChar() {
   ctx.beginPath();
-  ctx.arc(charPosX, charPosY, charRadius, 0, Math.PI * 2, false);
+  ctx.arc(char.posX, char.posY, charRadius, 0, Math.PI * 2, false);
   ctx.fillStyle = "#0095DD";
   ctx.fill();
   ctx.closePath();
 }
 
 function playerMovement() {
-  if (rightPressed && upPressed && charPosX < canvas.width && charPosY > 0) {
-    charPosX += 2;
-    charPosY -= 2;
-  } else if (leftPressed && upPressed && charPosX > 0 && charPosY > 0) {
-    charPosX -= 2;
-    charPosY -= 2;
-  } else if (rightPressed && downPressed && charPosX < canvas.width && charPosY < canvas.height) {
-    charPosX += 2;
-    charPosY += 2;
-  } else if (leftPressed && downPressed && charPosY < canvas.height && charPosX > 0) {
-    charPosX -= 2;
-    charPosY += 2;
-  } else if (rightPressed && charPosX < canvas.width) {
-    charPosX += 2;
-  } else if (leftPressed && charPosX > 0) {
-    charPosX -= 2;
-  } else if (upPressed && charPosY > 0) {
-    charPosY -= 2;
-  } else if (downPressed && charPosY < canvas.height) {
-    charPosY += 2;
+  if (rightPressed && upPressed && char.posX < canvas.width && char.posY > 0) {
+    char.posX += 2;
+    char.posY -= 2;
+  } else if (leftPressed && upPressed && char.posX > 0 && char.posY > 0) {
+    char.posX -= 2;
+    char.posY -= 2;
+  } else if (rightPressed && downPressed && char.posX < canvas.width && char.posY < canvas.height) {
+    char.posX += 2;
+    char.posY += 2;
+  } else if (leftPressed && downPressed && char.posY < canvas.height && char.posX > 0) {
+    char.posX -= 2;
+    char.posY += 2;
+  } else if (rightPressed && char.posX < canvas.width) {
+    char.posX += 2;
+  } else if (leftPressed && char.posX > 0) {
+    char.posX -= 2;
+  } else if (upPressed && char.posY > 0) {
+    char.posY -= 2;
+  } else if (downPressed && char.posY < canvas.height) {
+    char.posY += 2;
   }
 }
 
